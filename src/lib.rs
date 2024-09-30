f263f57769c62728e4817a4ec9c8465c1cf9f361
Ashai <nekumitsurugi@gmail.com>
2024-09-30 14:27:35 -0700

    config implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(ptr_sub_ptr)]
 
 use fps_config::fps_settings_callback;
+use mov_config::spd_settings_callback;
 use mov_config::mov_settings_callback;
 
 pub mod fps_config;
@@ -10,15 +11,18 @@ pub mod mov_config;
 pub mod utils;
 
 pub static mut CURRENT_FPS: i32 = 30;
+pub static mut ACCURATE_SPEED: bool = true;
 pub static mut ACCURATE_MOVEMENT: bool = true;
 
 #[skyline::main(name = "fps_settings_plugin")]
 pub fn main() {
     // Adds settings to the Settings menu, in-game.
     cobapi::install_game_setting(fps_settings_callback);
+    cobapi::install_game_setting(spd_settings_callback);
     cobapi::install_game_setting(mov_settings_callback);
     // Adds settings to the Global Settings menu, in the Cobalt menu (located on the title screen).
     cobapi::install_global_game_setting(fps_settings_callback);
+    cobapi::install_global_game_setting(spd_settings_callback);
     cobapi::install_global_game_setting(mov_settings_callback);
     skyline::install_hooks!(
         fps_hooks::vsync_count_hook,
